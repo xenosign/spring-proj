241239f719c53200abc50b30c2773b95e01086fc
kdtTetz <xenosign@naver.com>
2025-08-23 02:04:33 +0900

    Initial commit

---
+++ src/main/java/com/tetz/spring_proj/SpringProjApplication.java
@@ -0,0 +1,13 @@
+package com.tetz.spring_proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tetz/spring_proj/SpringProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tetz.spring_proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
