38bd6ef6961fbd5e38f1c8497e7fc67f117a2089
kdtTetz <xenosign@naver.com>
2025-09-16 00:56:49 +0900

    250916 홈에 스웨거 바로가기 추가

---
+++ src/main/java/com/tetz/spring_proj/HomeController.java
@@ -0,0 +1,28 @@
+package com.tetz.spring_proj;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+    @GetMapping(produces = "text/html")
+    public String home() {
+        return """
+            <!DOCTYPE html>
+            <html lang="ko">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>kyucchoi</title>
+            </head>
+            <body>
+                <a href="./swagger-ui/index.html" target="_blank">
+                    <h1>🚀 스웨거 바로가기</h1>
+                </a>
+            </body>
+            </html>
+            """;
+    }
+}
