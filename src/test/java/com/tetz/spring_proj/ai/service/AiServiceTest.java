6babf2174563e2e975fce1c208ddb26fad47dd1f
kdtTetz <xenosign@naver.com>
2025-10-26 18:59:13 +0900

    Create AiServiceTest.java

---
+++ src/test/java/com/tetz/spring_proj/ai/service/AiServiceTest.java
@@ -0,0 +1,16 @@
+package com.tetz.spring_proj.ai.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AiServiceTest {
+
+    @Test
+    void streamAiResponse() {
+    }
+
+    @Test
+    void streamComparedAiResponse() {
+    }
+}
