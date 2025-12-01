aa63ab35048f576afb52fba6c4843d687aa32898
xenosign <xenosign@naver.com>
2025-12-01 17:12:37 +0900

    feat: add front url in CORS configuration list

--- src/main/java/com/tetz/spring_proj/common/security/SecurityConfig.java
+++ src/main/java/com/tetz/spring_proj/common/security/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173", "http://127.0.0.1:5500", "http://127.0.0.1:5599"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173", "http://127.0.0.1:5500", "http://127.0.0.1:5599", "https://nextjs-practice-three-opal.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
