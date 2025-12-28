cbfd4a2d87160ec0bc535a871e3f041f6e8bcc5f
kdtTetz <xenosign@naver.com>
2025-12-28 22:28:53 +0900

    251228

---
+++ src/main/java/com/tetz/spring_proj/tetznight/controller/TetzNightController.java
@@ -0,0 +1,14 @@
+package com.tetz.spring_proj.tetznight.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/tetz")
+@RequiredArgsConstructor
+@Slf4j
+public class TetzNightController {
+
+}
